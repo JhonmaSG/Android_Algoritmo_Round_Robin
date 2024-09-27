b30a0eabdd7f5d7719cf8dece4432ea6ba9caa95
Furotsuki <juanpablodazab@gmail.com>
2024-09-26 20:58:21 -0500

    Agregamiento layout_principal y teoria del ejercicio

--- app/src/main/java/com/example/algoritmo_round_robin/MainActivity.kt
+++ app/src/main/java/com/example/algoritmo_round_robin/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.algoritmo_round_robin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,7 +14,11 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         /*Bienvenida al tutorial del Algoritmo de Planificación*/
-
+        val boton_comenzar = findViewById<Button>(R.id.boton_comenzar)
+        boton_comenzar.setOnClickListener {
+            val intent = Intent(this, activity_teorica_1::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/algoritmo_round_robin/activity_teorica_1.kt
+++ app/src/main/java/com/example/algoritmo_round_robin/activity_teorica_1.kt
@@ -1,6 +1,8 @@
 package com.example.algoritmo_round_robin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +16,11 @@ class activity_teorica_1 : AppCompatActivity() {
         /*Realizar un mini tutorial sobre el funcionamiento
         del algoritmo, se debe mostrar las formulas usadas
         para el proceso del calculo del algoritmo*/
+        val boton_comenzar = findViewById<Button>(R.id.pasar_proceso)
+        boton_comenzar.setOnClickListener {
+            val intent = Intent(this, activity_funcionales_1::class.java)
+            startActivity(intent)
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
