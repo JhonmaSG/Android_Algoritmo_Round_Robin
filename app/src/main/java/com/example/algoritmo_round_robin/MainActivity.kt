53e6af3248d0723cbbf6dddfcc3ede89ffc96cc2
JhonmaSG <jmserranog@udistrital.edu.co>
2024-09-26 22:20:35 -0500

    completed activity teorica_1

--- app/src/main/java/com/example/algoritmo_round_robin/MainActivity.kt
+++ app/src/main/java/com/example/algoritmo_round_robin/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.algoritmo_round_robin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,7 +14,11 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         /*Bienvenida al tutorial del Algoritmo de Planificación*/
+        val btncomenzar : Button = findViewById(R.id.btnComenzar);
 
-
+        btncomenzar.setOnClickListener{
+            val intentComenzar = Intent(this,activity_teorica_1::class.java).apply{}
+            startActivity(intentComenzar)
+        }
     }
 }
